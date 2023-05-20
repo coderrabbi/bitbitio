158983e6681346ae61bc304388c10cc88d24dcd2
coderrabbie <golamrabbi.dv@gmail.com>
2023-05-21 03:50:11 +0600

    v1.0.0

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
-window.scrollX();
+const faq = document.getElementsByClassName("faq-page");
+let i;
+for (i = 0; i < faq.length; i++) {
+  faq[i].addEventListener("click", function () {
+    this.classList.toggle("active");
+    var body = this.nextElementSibling;
+    if (body.style.display === "block") {
+      body.style.display = "none";
+      body.style.opacity = 0;
+    } else {
+      body.style.display = "block";
+      body.style.opacity = 1;
+    }
+  });
+}
